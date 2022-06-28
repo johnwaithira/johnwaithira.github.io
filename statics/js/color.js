a2aeedd6dd4fab52337968185c922047c3869f20
John Waithira <78137880+johnwaithira@users.noreply.github.com>
2022-06-28 17:43:04 +0300

    Update color.js

--- statics/js/color.js
+++ statics/js/color.js
@@ -7,3 +7,23 @@ function colorToHex(color)
 function toHexa(red, green, blue){
     return "#" + colorToHex(red) + colorToHex(green) + colorToHex(blue);
 }
+ function tab(evt, tabname, tabbtn, tabcontent)
+    {
+            var tabbutton, tabcontent, i;
+            tabcontent = document.getElementsByClassName(tabcontent)
+
+            for(i = 0; i< tabcontent.length; i++)
+            {
+                tabcontent[i].style.display = "none"
+            }
+            tabbutton = document.getElementsByClassName(tabbtn)
+
+            for(i = 0; i< tabbutton.length; i++)
+            {
+                tabbutton[i].className =  tabbutton[i].className.replace(" active", "")
+            }
+
+            document.getElementById(tabname).style.display = "block"
+            evt.currentTarget.className += " active"
+            
+        }
